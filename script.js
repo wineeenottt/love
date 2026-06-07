4455d2faa5d1bf9844629c3e125ba1fdce311e92
Maria Kosogova <466310@niuitmo.ru>
2026-06-07 13:56:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".yes-btn");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout(() => {
+        document.querySelector(".letter-window").classList.add("open");
+    }, 50);
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const min = 200;
+    const max = 200;
+
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
+
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
+
+let yesScale = 1;
+
+yesBtn.style.position = "relative";
+yesBtn.style.transformOrigin = "center center";
+yesBtn.style.transition = "transform 0.3s ease";
+
+noBtn.addEventListener("click", () => {
+    yesScale += 2;
+
+    if (yesBtn.style.position !== "fixed") {
+        yesBtn.style.position = "fixed";
+        yesBtn.style.top = "50%";
+        yesBtn.style.left = "50%";
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    } else {
+        yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    }
+});
+
+yesBtn.addEventListener("click", () => {
+    title.textContent = "Yippeeee!";
+    catImg.src = "cat_dance.gif";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+    finalText.style.display = "block";
+
+    let count = 7;
+    const countdownEl = document.getElementById("countdown");
+
+    const timer = setInterval(() => {
+        count--;
+        countdownEl.textContent = count;
+
+        if (count <= 0) {
+            clearInterval(timer);
+            window.location.href = "nextpage.html";
+        }
+    }, 1000);
+});
